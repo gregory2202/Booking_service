cf6aa125675d0daff8eccddb90caaf6b07d628a8
gregory.konovalov <gregory.konovalov.1999@mail.ru>
2023-09-02 20:51:55 +0300

    Added an abstract interface, renamed the base class

--- app/repository/base_repository.py
+++ app/repository/base_repository.py
@@ -1,28 +1,53 @@
+from abc import ABC, abstractmethod
+
 from sqlalchemy import select, insert, delete
 
 from app.database import async_session_maker
 
 
-class BaseDAO:
+class AbstractRepository(ABC):
+
+    @classmethod
+    @abstractmethod
+    async def find_all(cls, **kwargs):
+        raise NotImplemented
+
+    @classmethod
+    @abstractmethod
+    async def find_one_or_none(cls, **kwargs):
+        raise NotImplemented
+
+    @classmethod
+    @abstractmethod
+    async def add(cls, **kwargs):
+        raise NotImplemented
+
+    @classmethod
+    @abstractmethod
+    async def delete(cls, **kwargs):
+        raise NotImplemented
+
+
+class SQLAlchemyRepository(AbstractRepository):
     model = None
 
     @classmethod
     async def find_all(cls, **kwargs):
-        query = select(cls.model.__table__.columns).filter_by(**kwargs)
+        query = select(cls.model).filter_by(**kwargs)
         async with async_session_maker() as session:
             result = await session.execute(query)
             return result.mappings().all()
 
     @classmethod
     async def find_one_or_none(cls, **kwargs):
-        query = select(cls.model.__table__.columns).filter_by(**kwargs)
+        query = select(cls.model).filter_by(**kwargs)
         async with async_session_maker() as session:
             result = await session.execute(query)
-            return result.mappings().one_or_none()
+            return result.scalars().first()
 
     @classmethod
     async def add(cls, **kwargs):
-        query = insert(cls.model).values(**kwargs).returning(cls.model.id)
+        query = insert(cls.model).values(**kwargs).returning(cls.model)
         async with async_session_maker() as session:
             result = await session.execute(query)
             await session.commit()
