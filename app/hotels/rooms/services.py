ea98f822947bdf15b45c8ce7defee8648b83de7f
gregory.konovalov <gregory.konovalov.1999@mail.ru>
2023-09-04 22:06:07 +0300

    Fixed get_rooms_by_date

--- app/hotels/rooms/services.py
+++ app/hotels/rooms/services.py
@@ -7,5 +7,5 @@ class RoomsServices:
     def __init__(self, rooms_repository: RoomsRepository):
         self.rooms_repository = rooms_repository
 
-    async def get_rooms_by_date(self, hotel_id: int, date_from: date, date_to: date) -> list[SRoomInfo]:
+    async def get_rooms_by_date(self, hotel_id: int, date_from: date, date_to: date):
         return await self.rooms_repository.find_all(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
