70ed4c07aa77ce27fdcc747bf980cc500f6221f5
gregory.konovalov <gregory.konovalov.1999@mail.ru>
2023-09-14 10:25:53 +0300

    Refactored unit_of_work

---
+++ app/unit_of_work/unit_of_work.py
@@ -0,0 +1,34 @@
+from app.interfaces.unit_of_work import IUnitOfWork
+from app.repositories.bookings import SQLAlchemyBookingsRepository
+from app.repositories.hotels import SQLAlchemyHotelsRepository
+from app.repositories.rooms import SQLAlchemyRoomsRepository
+from app.repositories.users import SQLAlchemyUsersRepository
+
+
+class SQLAlchemyUnitOfWork(IUnitOfWork):
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        self.users_repository = SQLAlchemyUsersRepository(self.session)
+        self.bookings_repository = SQLAlchemyBookingsRepository(self.session)
+        self.hotels_repository = SQLAlchemyHotelsRepository(self.session)
+        self.rooms_repository = SQLAlchemyRoomsRepository(self.session)
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            await self.rollback()
+        else:
+            await self.session.commit()
+        await self.session.close()
+        return False
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
